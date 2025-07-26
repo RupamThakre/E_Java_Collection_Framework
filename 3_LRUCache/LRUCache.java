8f6e93c704dd73d1abac28b728fc712f143b2f05
RupamThakre <rupamthakre15@gmail.com>
2025-07-26 19:11:21 +0530

    Implement LRU Cache using LinkedHashMap

--- 3_LRUCache/LRUCache.java
+++ 3_LRUCache/LRUCache.java
@@ -6,13 +6,14 @@ class LRUCache<K,V> extends LinkedHashMap<K, V>{
     private int capacity;
 
     public LRUCache(int capacity){
+        // true = access-order (not insertion-order)
         super(capacity, 0.75f, true);
         this.capacity = capacity;
     }
-
+    // This method is called after each put or putAll
     @Override
     protected boolean removeEldestEntry(Map.Entry<K,V> eldest){
-        return size() > capacity;
+        return size() > capacity; // remove the eldest if size exceeds capacity
     }
 
     public static void main(String[] args) {
